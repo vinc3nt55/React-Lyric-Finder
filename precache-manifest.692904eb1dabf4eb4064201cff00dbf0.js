8bbb38b3a69eed90aa960bf8e9035ddce05a71b0
Rockei17 <rockie.rose143@gmail.com>
2019-03-06 12:46:24 +0800

    Updates

---
+++ precache-manifest.692904eb1dabf4eb4064201cff00dbf0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2943cf5a126a124ef44a",
+    "url": "/React-Lyric-Finder/static/js/runtime~main.2943cf5a.js"
+  },
+  {
+    "revision": "19a00ef6177113669847",
+    "url": "/React-Lyric-Finder/static/js/main.19a00ef6.chunk.js"
+  },
+  {
+    "revision": "57bf4c21c9690616c822",
+    "url": "/React-Lyric-Finder/static/js/1.57bf4c21.chunk.js"
+  },
+  {
+    "revision": "19a00ef6177113669847",
+    "url": "/React-Lyric-Finder/static/css/main.ce73850b.chunk.css"
+  },
+  {
+    "revision": "797757afff05a328ce10a0700d787451",
+    "url": "/React-Lyric-Finder/index.html"
+  }
+];
